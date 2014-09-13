fe63f45c949f6b41dd74deb6899c5112d0a8fcc9
Mário Filipe Farate Marques <MarioMarques@mbp-de-mario.lan>
2014-09-13 19:21:56 +0100

    Initial Commit

---
+++ FruitSalad/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  FruitSalad
+//
+//  Created by Mário Filipe Farate Marques on 13/09/14.
+//  Copyright MM 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ FruitSalad/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FruitSalad
+//
+//  Created by Mário Filipe Farate Marques on 13/09/14.
+//  Copyright MM 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
